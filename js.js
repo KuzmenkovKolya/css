90af3b757b28b63389f2a391be96b49af1ac6d02
KuzmenkovKolya <117455695+KuzmenkovKolya@users.noreply.github.com>
2022-11-22 14:37:10 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,4 +1,83 @@
+//переменные
 
-const b = prompt("2021-2001= введите число")
-if (b == 20) {alert("Молодец")}
-else {alert("Ты ПИДОР")}
+//const lastName ='Kuzmenkov'//string
+//let age = 20
+//const isdown = true //boolean
+
+//console.log(lastName)
+
+//2 Мутирование
+
+ //const name = prompt('введите имя ')
+ //alert(name +' '+ lastName)
+ 
+ //3 операторы
+ //let currentYear = 2022
+ //const birthYear = 2001
+ //const age = currentYear-birthYear
+ //console.log(age)
+ //const a=11
+ //const b=12
+ //let c=32
+ //c = c + a
+ //console.log(a + b)
+ //console.log(a - b)
+ //console.log(a * b)
+ //console.log(a / b)
+ //console.log(currentYear++)
+ 
+
+ //4 типы данных
+ //const isProgrammer = true
+ //const name = 'kolya'
+// const age = 20
+// let x
+ 
+ //console.log(typeof isProgrammer)
+ //console.log(typeof name)
+ //console.log(typeof age)
+ //console.log(typeof x)
+ //console.log(null)
+  
+
+ //5 Приоритет операторов
+ //const fullAge = 20
+ //const birthYear = 2001
+ //const currentYear = 2022
+ //const isFullAge = currentYear - birthYear >= fullAge
+ //console.log(isFullAge)
+ 
+ 
+ //6 УСЛОВНЫЕ ОПЕРАТОРЫ 
+//const eduacation = 'pending'
+ //if (eduacation === 'ready') {
+ //   console.log('я мегамозг')
+
+ //}
+//else  if (eduacation === 'pending'){
+   // console.log('я пытаюсь')
+// }
+
+ //const isReady = true
+ //if (isReady === true){
+  //  console.log('ВСЁ Готво!!!')
+ //}
+ //else(isReady ===false){
+   // console.log('Всё не готово!')
+ //}
+ //const num1 = 42
+ //const num2 = '42'
+ //console.log(num1 === num2)
+
+
+ //7 Булевая Лог
+const name = prompt('как вас зовут?')
+if ( name === 'Slava'  ) 
+    alert('Спасибо что не соврал')
+if ( name ==='slava') 
+    alert('Спасибо что не соврал')
+if (name === 'Слава') alert('Спасибо что не соврал')
+if (name === 'Славик') alert('Спасибо что не соврал')
+if (name === 'слава') alert('Спасибо что не соврал')
+if (name === 'славик') alert('Спасибо что не соврал')
+else alert('Врёшь!!!')
